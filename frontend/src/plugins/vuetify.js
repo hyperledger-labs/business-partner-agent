0f38ddd9e421f480831122bfd7b9dbc6a6bea8f5
Dominic Wörner <dominic.woerner2@ch.bosch.com>
2020-09-29 12:14:21 +0200

    Change UI color in settings. Persist in localStorage (#131)
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
    Signed-off-by: Vitali Fuhrmann <mail@fuhrmann-se.de>

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -16,6 +16,11 @@ if (uiColor) {
     primaryColor = uiColor;
 }
 
+const uiColor = localStorage.getItem("uiColor");
+if (uiColor) {
+  primaryColor = uiColor
+}
+
 export default new Vuetify({
     icons: {
         iconfont: "mdiSvg",
