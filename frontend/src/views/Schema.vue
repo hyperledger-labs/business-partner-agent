36d153a2c05fe9718affb15c28140a68103f85d4
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2020-10-02 17:10:44 +0200

    fix schemaService and add schema details
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- frontend/src/views/Schema.vue
+++ frontend/src/views/Schema.vue
@@ -7,18 +7,41 @@
 -->
 <template>
 <v-container justify-center>
-    <v-card class="mx-auto" flat>
+    <v-card v-if="!isLoading" class="mx-auto" flat>
         <v-card-title class="bg-light">
             <v-btn depressed color="secondary" icon @click="$router.go(-1)">
                 <v-icon dark>mdi-chevron-left</v-icon>
             </v-btn>
             <span>{{data.label}}</span>
-            <v-btn icon @click="deleteSchema">
-                <v-icon>
-                    mdi-delete
-                </v-icon>
-            </v-btn>
+            <v-layout align-end justify-end>
+                <!-- <v-btn depressed icon @click="isUpdatingName = !isUpdatingName">
+                    <v-icon dark>mdi-pencil</v-icon>
+                </v-btn> -->
+                <v-btn depressed color="red" icon @click="deleteSchema">
+                    <v-icon dark>mdi-delete</v-icon>
+                </v-btn>
+            </v-layout>
+
         </v-card-title>
+
+        <v-list-item class="mt-4">
+            <v-list-item-title class="grey--text text--darken-2 font-weight-medium">
+                Schema ID
+            </v-list-item-title>
+        <v-list-item-subtitle>
+            {{data.schemaId}}
+        </v-list-item-subtitle>
+        </v-list-item>
+
+        <h4 class="my-4 grey--text text--darken-3">Schema Attributes</h4>
+
+        <v-list-item v-for="attribute in data.schemaAttributeNames" :key="attribute.id">
+
+            <p class="grey--text text--darken-2 font-weight-medium">
+                {{attribute}}
+            </p>
+
+        </v-list-item>
 
     </v-card>
 </v-container>
@@ -30,18 +53,25 @@ import {
 } from "../main";
 export default {
     name: "Schema",
-     props: {
+    props: {
         id: String, //schema ID
+        schema: Object
     },
     created() {
         EventBus.$emit("title", "Schema");
-        this.fetch()
+
+        if (this.schema) {
+            this.isLoading = false
+            this.data = this.schema;
+        } else {
+            this.fetch();
+        }
     },
     data: () => {
         return {
             data: [],
-            isBusy: true,
-            
+            isLoading: true,
+
         };
     },
     computed: {
@@ -49,20 +79,20 @@ export default {
     },
     methods: {
         fetch() {
-            this.$axios.get(`${this.$apiBaseUrl}/schema/${this.id}`)
+            this.isLoading = true;
+            this.$axios.get(`${this.$apiBaseUrl}/admin/schema/${this.id}`)
                 .then((result) => {
                     console.log(result);
                     if ({}.hasOwnProperty.call(result, 'data')) {
 
-                        this.isBusy = false
-
-                        this.data = result.data
+                        this.data = result.data;
+                        this.isLoading = false;
 
                     }
                 })
                 .catch((e) => {
 
-                    this.isBusy = false
+                    this.isLoading = false
                     if (e.response.status === 404) {
 
                         this.data = []
@@ -74,7 +104,7 @@ export default {
 
                 });
         },
-        
+
         deleteSchema() {
             this.$axios
                 .delete(`${this.$apiBaseUrl}/admin/schema/${this.id}`)
@@ -82,7 +112,9 @@ export default {
                     console.log(result);
                     if (result.status === 200) {
                         EventBus.$emit("success", "Schema deleted");
-                        this.$router.push({ name: "SchemaSettings"})
+                        this.$router.push({
+                            name: "SchemaSettings"
+                        })
                     }
                 })
                 .catch(e => {
