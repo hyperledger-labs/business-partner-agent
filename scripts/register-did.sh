a6727e90f176c5df1f5bf9d13a0990a0a48bb922
Dominic Wörner <dominic.woerner2@ch.bosch.com>
2020-10-11 11:34:50 +0200

    Feature/135 improve script (#158)
    * Improve script to save seed to .env file. Quickstart in readme
    
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- scripts/register-did.sh
+++ scripts/register-did.sh
@@ -4,19 +4,33 @@
 # 
 # SPDX-License-Identifier: Apache-2.0
 
+#!/bin/bash
+
 # Set URL
 URL=https://indy-test.bosch-digital.de
+
+# Set random alias
 ALIAS=BPA-$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 # Generate random seed
 SEED=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 PAYLOAD='{"alias":"'"$ALIAS"'","seed":"'"$SEED"'","role":"ENDORSER"}'
 
+# Register DID
 if curl --fail -s -d $PAYLOAD  -H "Content-Type: application/json" -X POST ${URL}/register; then
     # Registration (probably) successfull
     echo ""
-    echo Registration on $URL successful
-    echo Please copy AGENT_SEED=$SEED to your .env file
+    echo ""Registration on $URL successful""
+    echo ""Setting AGENT_SEED in .env file""
+    if [ ! -f .env ]; then
+        echo "".env does not exist""
+        echo ""Creating .env from .env-example""
+        cp .env-example .env
+    fi
+    #sed -i "" 's/AGENT_SEED .*$/AGENT_SEED="$SEED"/' .env
+    sed -i '' '/AGENT_SEED=/c\
+    AGENT_SEED='"${SEED}"'
+    ' .env
 else
     # Something went wrong
     echo ""
