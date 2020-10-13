e5e050764ec77f3e0298223e87598f233271b84e
Dominic Wörner <dominic.woerner2@ch.bosch.com>
2020-10-13 18:53:12 +0200

    show all states. disable click for incomplete states. allow deletion (#171)
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- frontend/src/components/PresentationList.vue
+++ frontend/src/components/PresentationList.vue
@@ -7,31 +7,22 @@
 -->
 
 <template>
-<v-data-table 
-    :hide-default-footer="credentialsWithIndex.length < 10" 
-    v-model="selected" 
-    :show-select="selectable" 
-    single-select 
-    :headers="headers" 
-    :items="credentialsWithIndex" 
-    :expanded.sync="expanded" 
-    item-key="index" 
-    :show-expand="expandable"
-    @click:row="openPresentation"
->
+<v-data-table :hide-default-footer="credentialsWithIndex.length < 10" v-model="selected" :show-select="selectable" single-select :headers="headers" :items="credentialsWithIndex" :expanded.sync="expanded" item-key="index" :show-expand="expandable" @click:row="openPresentation">
     <template v-slot:[`item.type`]="{ item }">
         <div v-if="item.type === CredentialTypes.OTHER.name">{{ item.credentialDefinitionId | credentialTag }}</div>
         <div v-else>{{ item.type | credentialLabel }}</div>
     </template>
     <template v-slot:[`item.state`]="{ item }">
         <v-icon v-if="item.state === 'verified' || item.state== 'presentation_acked'" color="green">mdi-check</v-icon>
-        <!-- <v-btn v-if="item.indyCredential" color="primary" text>verify</v-btn> -->
+        <span v-else>
+            {{ item.state.replace("_", " ")}}
+        </span>
     </template>
     <template v-slot:[`item.sentAt`]="{ item }">
-       {{item.sentAt | moment("YYYY-MM-DD HH:mm") }}
+        {{item.sentAt | moment("YYYY-MM-DD HH:mm") }}
     </template>
     <template v-slot:[`item.receivedAt`]="{ item }">
-       {{item.receivedAt | moment("YYYY-MM-DD HH:mm") }}
+        {{item.receivedAt | moment("YYYY-MM-DD HH:mm") }}
     </template>
     <template v-slot:expanded-item="{ headers, item }">
         <td :colspan="headers.length">
@@ -39,12 +30,9 @@
         </td>
     </template>
     <template v-slot:[`item.actions`]="{ item }">
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+        <v-icon small @click="deletePresentation(item)">
+            mdi-delete
+        </v-icon>
     </template>
 </v-data-table>
 </template>
@@ -60,7 +48,7 @@ import {
 export default {
     props: {
         credentials: Array,
-         selectable: {
+        selectable: {
             type: Boolean,
             default: false
         },
@@ -86,15 +74,14 @@ export default {
                     text: "State",
                     value: "state"
                 },
-                // {
-                //     text: "Actions",
-                //     value: "actions"
-                // }
+                {
+                    text: " ",
+                    value: "actions"
+                }
             ]
         }
     },
-    created() {
-    },
+    created() {},
     data: () => {
         return {
             selected: [],
@@ -117,12 +104,43 @@ export default {
         }
     },
     methods: {
-         openPresentation(presentation) {
-            if (presentation.id) {
-                this.$router.push({ path: `presentation/${presentation.id}`, append: true})
+        deletePresentation(presentation) {
+            console.log(presentation)
+            let partnerId = this.$route.params.id;
+            this.$axios
+                .delete(`${this.$apiBaseUrl}/partners/${partnerId}/proof/${presentation.id}`)
+                .then(result => {
+                    if (result.status === 200) {
+                        this.credentials = this.credentials.filter( pres => {
+                            pres.id !== presentation;
+                        });
+                    }
+                })
+                .catch(e => {
+                    console.error(e);
+                    EventBus.$emit("error", e);
+                });
+        },
+        openPresentation(presentation) {
+            if (presentation.state === 'verified' || presentation.state == 'presentation_acked') {
+
+                if (presentation.id) {
+                    this.$router.push({
+                        path: `presentation/${presentation.id}`,
+                        append: true
+                    })
+
+                } else {
+                    EventBus.$emit('error', 'No details view available for presentations in public profile.')
+                }
+
             } else {
-                EventBus.$emit('error', 'No details view available for presentations in public profile.')
+
+                // Do nothing for now. Presentation is not ready
+                // Need to fix Presentation.vue for unfinished presentations
+
             }
+
         }
     },
     components: {
