116c3a2ac496b6f0c9441206fbed15c5f37a426c
Vitali Fuhrmann <mail@fuhrmann-se.de>
2020-10-14 14:19:02 +0200

    Merge
    Signed-off-by: Vitali Fuhrmann <mail@fuhrmann-se.de>

--- frontend/src/views/Credential.vue
+++ frontend/src/views/Credential.vue
@@ -6,7 +6,6 @@
  SPDX-License-Identifier: Apache-2.0
 -->
 <template>
-<<<<<<< HEAD
     <v-card v-if="isReady" class="mx-auto">
         <v-card-title class="bg-light">
             <v-btn
@@ -61,48 +60,6 @@
                 </v-expansion-panel>
             </v-expansion-panels>
         </v-card-actions>
-=======
-  <v-card v-if="isReady" class="mx-auto">
-    <v-card-title class="bg-light">
-      <v-btn depressed color="secondary" icon @click="$router.push({ name: 'Wallet' })">
-        <v-icon dark>mdi-chevron-left</v-icon>
-      </v-btn>
-      <div v-if="credential.type === CredentialTypes.OTHER.name" >{{ credential.credentialDefinitionId | credentialTag }}</div>
-      <div v-else>{{ credential.type | credentialLabel }}</div>
-      <v-layout align-end justify-end>
-        <v-btn depressed color="red" icon @click="deleteCredential()">
-          <v-icon dark>mdi-delete</v-icon>
-        </v-btn>
-      </v-layout>
-    </v-card-title>
-    <v-card-text>
-      <Cred v-bind:document="credential" isReadOnly></Cred>
-      <v-divider></v-divider>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Public Profile</v-list-item-title>
-          <v-list-item-subtitle>Visible in Public Profile</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-           <v-switch :disabled="credential.type === CredentialTypes.OTHER.name" 
-            v-model="isPublic"></v-switch>
-        </v-list-item-action>
-      </v-list-item>
-      <v-divider></v-divider>
-    </v-card-text>
-    <v-card-actions>
-      <v-expansion-panels v-if="expertMode" accordion flat>
-        <v-expansion-panel>
-          <v-expansion-panel-header
-            class="grey--text text--darken-2 font-weight-medium bg-light"
-          >Show raw data</v-expansion-panel-header>
-          <v-expansion-panel-content class="bg-light">
-            <vue-json-pretty :data="credential"></vue-json-pretty>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-card-actions>
->>>>>>> f47c467... Disable 'add to public profile' of credentials of type 'OTHER' (#134)
 
         <v-card-actions>
             <v-layout align-end justify-end>
@@ -121,50 +78,14 @@
 
 <script>
 import { EventBus } from "../main";
-
 import Cred from "@/components/Credential";
 import VueJsonPretty from "vue-json-pretty";
 import { CredentialTypes } from "../constants";
-
 export default {
-  name: "Credential",
-  props: {
-    id: String,
-    type: String
-  },
-  created() {
-    EventBus.$emit("title", "Credential");
-    this.getCredential();
-  },
-  data: () => {
-    return {
-      document: {},
-      isBusy: false,
-      isReady: false,
-      CredentialTypes: CredentialTypes
-    };
-  },
-  computed: {
-    expertMode() {
-      return this.$store.state.expertMode;
-    }
-  },
-  methods: {
-    getCredential() {
-      console.log(this.id);
-      this.$axios
-        .get(`${this.$apiBaseUrl}/wallet/credential/${this.id}`)
-        .then(result => {
-          if ({}.hasOwnProperty.call(result, "data")) {
-            this.credential = result.data;
-            this.isPublic = this.credential.isPublic;
-            this.isReady = true;
-          }
-        })
-        .catch(e => {
-          console.error(e);
-          EventBus.$emit("error", e);
-        });
+    name: "Credential",
+    props: {
+        id: String,
+        type: String,
     },
     created() {
         EventBus.$emit("title", "Credential");
@@ -260,7 +181,6 @@ export default {
 .bg-light {
     background-color: #fafafa;
 }
-
 .bg-light-2 {
     background-color: #ececec;
 }
--- frontend/src/views/PublicProfile.vue
+++ frontend/src/views/PublicProfile.vue
@@ -6,54 +6,69 @@
  SPDX-License-Identifier: Apache-2.0
 -->
 <template>
-  <v-container>
-    <div v-if="document">
-        <OganizationalProfile v-bind:document="document" isReadOnly></OganizationalProfile>
-        <DocumentCredentialList v-if="!isBusy" v-bind:credentials="publicDocumentsAndCredentials" isReadOnly showOnlyContent></DocumentCredentialList>
-    </div>
-      <v-container v-else fill-height fluid text-center>
-      <v-row align="center" justify="center">
-        <v-col>
-          <h1 class="grey--text text--lighten-2">You don't have set up a public profile yet</h1>
-          <v-btn color="primary" :to="{ name: 'Wallet' }" text>Setup your Profile</v-btn>
-        </v-col>
-      </v-row>
+    <v-container>
+        <div
+            v-if="
+                (document && document.isPublic) || publicDocumentsAndCredentials
+            "
+        >
+            <OrganizationalProfile
+                v-if="document && document.isPublic"
+                v-bind:document="document.documentData"
+                isReadOnly
+            >
+            </OrganizationalProfile>
+            <DocumentCredentialList
+                v-if="!isBusy"
+                v-bind:credentials="publicDocumentsAndCredentials"
+                isReadOnly
+                showOnlyContent
+            >
+            </DocumentCredentialList>
+        </div>
+
+        <v-container v-else fill-height fluid text-center>
+            <v-row align="center" justify="center">
+                <v-col>
+                    <h1 class="grey--text text--lighten-2">
+                        You don't have set up a public profile yet
+                    </h1>
+                    <v-btn color="primary" :to="{ name: 'Wallet' }" text
+                        >Setup your Profile</v-btn
+                    >
+                </v-col>
+            </v-row>
+        </v-container>
     </v-container>
 </template>
 
 <script>
-import OganizationalProfile from "@/components/OrganizationalProfile";
+import OrganizationalProfile from "@/components/OrganizationalProfile";
 import DocumentCredentialList from "@/components/credentials/DocumentCredentialList";
-
 import { EventBus } from "../main";
 export default {
-  name: "PublicProfile",
-  props: {},
-  components: {
-    OganizationalProfile,
-    DocumentCredentialList
-  },
-  computed: {
-    document() {
-      return this.$store.getters.organizationalProfile;
+    name: "PublicProfile",
+    props: {},
+    components: {
+        OrganizationalProfile,
+        DocumentCredentialList,
+    },
+    computed: {
+        document() {
+            return this.$store.getters.organizationalProfile;
+        },
+        isBusy() {
+            return this.$store.getters.isBusy;
+        },
+        publicDocumentsAndCredentials() {
+            console.log(this.$store.getters.publicDocumentsAndCredentials);
+            return this.$store.getters.publicDocumentsAndCredentials;
+        },
     },
     created() {
         EventBus.$emit("title", "Public Profile");
         this.$store.dispatch("loadDocuments");
         this.$store.dispatch("loadCredentials");
     },
-<<<<<<< HEAD
-=======
-    publicDocumentsAndCredentials() {
-      console.log(this.$store.getters.publicDocumentsAndCredentials);
-      return this.$store.getters.publicDocumentsAndCredentials;
-    }
-  },
-  created() {
-    EventBus.$emit("title", "Public Profile");
-    this.$store.dispatch("loadDocuments");
-    this.$store.dispatch("loadCredentials");
-  }
->>>>>>> 2b74dc7... feature/017 AddPartner, Partner View
 };
 </script>
