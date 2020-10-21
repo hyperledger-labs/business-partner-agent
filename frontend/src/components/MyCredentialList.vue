6fd93d6db5f22d70e1a68baaaf3f9662c43e59f4
Vitali Fuhrmann <vitali.fuhrmann@moenkemoeller.it>
2020-10-21 13:37:04 +0200

    feature/181 sorting
    Signed-off-by: Vitali Fuhrmann <vitali.fuhrmann@moenkemoeller.it>

--- frontend/src/components/MyCredentialList.vue
+++ frontend/src/components/MyCredentialList.vue
@@ -16,6 +16,8 @@
       :items="data"
       :show-select="selectable"
       single-select
+      :sort-by="['type', 'createdDate']"
+      :sort-desc="[true, true]"
       @click:row="open"
     >
       <template v-slot:[`item.type`]="{ item }">
