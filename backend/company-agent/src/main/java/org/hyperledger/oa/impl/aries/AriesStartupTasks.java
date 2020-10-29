b35ca1086ce4183052071b84765f424fecd10467
Frank Bernhardt <Frank.Bernhardt@bosch.com>
2020-10-29 16:06:02 +0100

    fix pmd
    Signed-off-by: Frank Bernhardt <Frank.Bernhardt@bosch.com>

--- backend/company-agent/src/main/java/org/hyperledger/oa/impl/aries/AriesStartupTasks.java
+++ backend/company-agent/src/main/java/org/hyperledger/oa/impl/aries/AriesStartupTasks.java
@@ -60,9 +60,6 @@ public class AriesStartupTasks {
     @Inject
     Optional<PartnerCredDefLookup> credLookup;
 
-    @Value("${oagent.web.only}")
-    private Boolean webOnly;
-
     @Async
     public void onServiceStartedEvent() {
         log.debug("Running aries startup tasks...");
