912fdd5bf94b96be5c400c0f236170eee39e36c8
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-03-16 11:26:33 +0100

    trusted issuer management
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- frontend/src/views/Schema.vue
+++ frontend/src/views/Schema.vue
@@ -50,7 +50,73 @@
             {{ attribute }}
           </p>
         </v-list-item>
+
+        <h4 class="my-4 grey--text text--darken-3">Trusted Issuers</h4>
+
+        <v-row v-for="(entry, index) in trustedIssuers" v-bind:key="index">
+          <v-col cols="4" class="py-0">
+            <v-text-field
+              label="DID"
+              :disabled="entry.readOnly || !entry.isEdit"
+              v-model="entry.issuerDid"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field
+              label="Name"
+              :disabled="entry.readOnly || !entry.isEdit"
+              v-model="entry.label"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+          <v-col cols="3" class="py-0">
+            <v-btn
+              v-if="!entry.readOnly && !entry.isEdit"
+              :disabled="isEdit"
+              color="primary"
+              text
+              @click="editTrustedIssuer(index)"
+              >Edit</v-btn
+            >
+
+            <v-btn
+              v-if="!entry.readOnly && entry.isEdit"
+              :loading="isBusy"
+              color="primary"
+              text
+              @click="saveTrustedIssuer(entry)"
+              >Save</v-btn
+            >
+
+            <v-btn
+              v-if="!entry.readOnly && entry.isEdit"
+              color="secondary"
+              text
+              @click="cancelEditTrustedIssuer(index)"
+              >Cancel</v-btn
+            >
+
+            <v-btn
+              icon
+              v-if="!entry.readOnly && !entry.isEdit"
+              @click="deleteTrustedIssuer(index)"
+            >
+              <v-icon color="error">mdi-delete</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-btn :disabled="isEdit" color="primary" text @click="addTrustedIssuer"
+          >Add</v-btn
+        >
       </v-container>
+      <v-card-actions>
+        <v-layout align-end justify-end>
+          <v-btn color="primary" text @click="cancel()">Close</v-btn>
+        </v-layout>
+      </v-card-actions>
     </v-card>
   </v-container>
 </template>
@@ -76,7 +142,16 @@ export default {
   data: () => {
     return {
       data: [],
+      isEdit: false,
+      editingTrustedIssuer: {},
       isLoading: true,
+      isBusy: false,
+      trustedIssuers: [
+        {
+          issuerDid: "",
+          label: "",
+        },
+      ],
     };
   },
   computed: {},
@@ -89,6 +164,10 @@ export default {
           console.log(result);
           if ({}.hasOwnProperty.call(result, "data")) {
             this.data = result.data;
+            // Init restrictions
+            if ({}.hasOwnProperty.call(this.data, "trustedIussuers")) {
+              this.trustedIssuers = this.data.trustedIssuers;
+            }
             this.isLoading = false;
           }
         })
@@ -100,6 +179,98 @@ export default {
             console.error(e);
             EventBus.$emit("error", e);
           }
+        });
+    },
+    addTrustedIssuer() {
+      this.isEdit = true;
+      this.trustedIssuers.push({
+        issuerDid: "",
+        label: "",
+        isEdit: true,
+      });
+    },
+    editTrustedIssuer(index) {
+      this.editingTrustedIssuer = Object.assign({}, this.trustedIssuers[index]);
+      this.isEdit = true;
+      this.trustedIssuers[index].isEdit = true;
+    },
+    cancelEditTrustedIssuer(index) {
+      this.trustedIssuers[index] = this.editingTrustedIssuer;
+      this.isEdit = false;
+      this.trustedIssuers[index].isEdit = false;
+    },
+    deleteTrustedIssuer(index) {
+      let trustedIssuer = this.trustedIssuers[index];
+      this.$axios
+        .delete(
+          `${this.$apiBaseUrl}/admin/schema/${this.id}/trustedIssuer/${trustedIssuer.id}`
+        )
+        .then((result) => {
+          console.log(result);
+          this.trustedIssuers.splice(index, 1);
+        })
+        .catch((e) => {
+          console.error(e);
+          EventBus.$emit("error", e);
+        });
+    },
+
+    saveTrustedIssuer(trustedIssuer) {
+      // delete trustedIssuer.isEdit;
+      if (trustedIssuer.id) {
+        this.updateTrustedIssuer(trustedIssuer);
+      } else {
+        this.createNewTrustedIssuer(trustedIssuer);
+      }
+    },
+
+    createNewTrustedIssuer(trustedIssuer) {
+      this.isBusy = true;
+
+      this.$axios
+        .post(
+          `${this.$apiBaseUrl}/admin/schema/${this.id}/trustedIssuer`,
+          trustedIssuer
+        )
+        .then((result) => {
+          console.log(result);
+          this.isBusy = false;
+
+          if (result.status === 200) {
+            (this.isEdit = false),
+              (trustedIssuer.isEdit = false),
+              EventBus.$emit("success", "New trusted issuer added");
+          }
+        })
+        .catch((e) => {
+          this.isBusy = false;
+          console.error(e);
+          EventBus.$emit("error", e);
+        });
+    },
+
+    updateTrustedIssuer(trustedIssuer) {
+      this.isBusy = true;
+
+      this.$axios
+        .put(
+          `${this.$apiBaseUrl}/admin/schema/${this.id}/trustedIssuer/${this.trustedIssuer.id}`,
+          trustedIssuer
+        )
+        .then((result) => {
+          console.log(result);
+          this.isBusy = false;
+          trustedIssuer.isEdit = false;
+
+          if (result.status === 201) {
+            EventBus.$emit("success", "Trusted issuer updated");
+          }
+        })
+        .catch((e) => {
+          this.isBusy = false;
+          trustedIssuer.isEdit = false;
+          console.error(e);
+          EventBus.$emit("error", e);
         });
     },
 
