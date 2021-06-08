86b02ee6cff48b7dcb9d1400839bcdd3186c71c1
Frank Bernhardt <Frank.Bernhardt@bosch.com>
2021-06-08 08:15:53 +0200

    add lic header
    Signed-off-by: Frank Bernhardt <Frank.Bernhardt@bosch.com>

--- backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyHealthCheck.java
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyHealthCheck.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020-2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/business-partner-agent
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hyperledger.bpa.client;
 
 import io.micronaut.context.annotation.Requires;
@@ -16,26 +33,26 @@ import org.hyperledger.aries.api.server.AdminStatusReadiness;
 @Requires(beans = HealthEndpoint.class)
 @Requires(property = HealthEndpoint.PREFIX + ".acapy.enabled", value = "true")
 public class AcaPyHealthCheck extends AbstractHealthIndicator<Map<String, String>> {
-  @Inject
-  AriesClient ac;
+    @Inject
+    AriesClient ac;
 
-  @Override
-  protected Map<String, String> getHealthInformation() {
-    try {
-      Optional<AdminStatusReadiness> status = ac.statusReady();
-      if (status.isPresent() && status.get().isReady()) {
-        this.healthStatus = HealthStatus.UP;
-      } else {
-        this.healthStatus = HealthStatus.DOWN;
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    @Override
+    protected Map<String, String> getHealthInformation() {
+        try {
+            Optional<AdminStatusReadiness> status = ac.statusReady();
+            if (status.isPresent() && status.get().isReady()) {
+                this.healthStatus = HealthStatus.UP;
+            } else {
+                this.healthStatus = HealthStatus.DOWN;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
-    return null;
-  }
 
-  @Override
-  protected String getName() {
-    return "aca-py";
-  }
+    @Override
+    protected String getName() {
+        return "aca-py";
+    }
 }
