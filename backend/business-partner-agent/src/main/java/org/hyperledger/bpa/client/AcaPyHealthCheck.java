a2647ef706b0de3196428c27f2988eb21c022975
Frank Bernhardt <Frank.Bernhardt@bosch.com>
2021-06-08 07:41:26 +0200

    fix pmd
    Signed-off-by: Frank Bernhardt <Frank.Bernhardt@bosch.com>

--- backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyHealthCheck.java
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyHealthCheck.java
@@ -29,7 +29,7 @@ public class AcaPyHealthCheck extends AbstractHealthIndicator<Map<String, String
         this.healthStatus = HealthStatus.DOWN;
       }
     } catch (IOException e) {
-      throw new RuntimeException((e));
+      throw new RuntimeException(e);
     }
     return null;
   }
