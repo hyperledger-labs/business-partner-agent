6e0eda457272794fcf18d42fbf86984e448e911c
frank-bee <Frank.Bernhardt@bosch.com>
2021-06-21 11:53:28 +0000

    Fix Health Check Fixes #482
    Signed-off-by: frank-bee <Frank.Bernhardt@bosch.com>

--- backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyLivenessCheck.java
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyLivenessCheck.java
@@ -17,42 +17,28 @@
  */
 package org.hyperledger.bpa.client;
 
+import java.util.Map;
+
+import javax.inject.Singleton;
+
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.health.HealthStatus;
 import io.micronaut.management.endpoint.health.HealthEndpoint;
 import io.micronaut.management.health.indicator.AbstractHealthIndicator;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import org.hyperledger.aries.AriesClient;
-import org.hyperledger.aries.api.server.AdminStatusReadiness;
+import io.micronaut.management.health.indicator.annotation.Liveness;
 
 @Singleton
+@Liveness
 @Requires(beans = HealthEndpoint.class)
-@Requires(property = HealthEndpoint.PREFIX + ".acapy.enabled", value = "true")
-public class AcaPyHealthCheck extends AbstractHealthIndicator<Map<String, String>> {
-    @Inject
-    AriesClient ac;
-
+public class AcaPyLivenessCheck extends AbstractHealthIndicator<Map<String, String>> {
     @Override
     protected Map<String, String> getHealthInformation() {
-        try {
-            Optional<AdminStatusReadiness> status = ac.statusReady();
-            if (status.isPresent() && status.get().isReady()) {
-                this.healthStatus = HealthStatus.UP;
-            } else {
-                this.healthStatus = HealthStatus.DOWN;
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        this.healthStatus = HealthStatus.UP;
         return null;
     }
 
     @Override
     protected String getName() {
-        return "aca-py";
+        return "aca-py-liveness";
     }
 }
---
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyReadinessCheck.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020-2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/business-partner-agent
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hyperledger.bpa.client;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.hyperledger.aries.AriesClient;
+import org.hyperledger.aries.api.server.AdminStatusReadiness;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.health.HealthStatus;
+import io.micronaut.management.endpoint.health.HealthEndpoint;
+import io.micronaut.management.health.indicator.AbstractHealthIndicator;
+import io.micronaut.management.health.indicator.annotation.Readiness;
+
+@Singleton
+@Readiness
+@Requires(beans = HealthEndpoint.class)
+public class AcaPyReadinessCheck extends AbstractHealthIndicator<Map<String, String>> {
+    @Inject
+    AriesClient ac;
+
+    @Override
+    protected Map<String, String> getHealthInformation() {
+        try {
+            Optional<AdminStatusReadiness> status = ac.statusReady();
+            if (status.isPresent() && status.get().isReady()) {
+                this.healthStatus = HealthStatus.UP;
+            } else {
+                this.healthStatus = HealthStatus.DOWN;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    protected String getName() {
+        return "aca-py-readiness";
+    }
+}
