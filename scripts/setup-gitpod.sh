17201dcf7736896a97f8800763e620d25cae038b
frank-bee <Frank.Bernhardt@bosch.com>
2021-06-25 12:16:02 +0000

    fix env vars
    Signed-off-by: frank-bee <Frank.Bernhardt@bosch.com>

--- scripts/setup-gitpod.sh
+++ scripts/setup-gitpod.sh
@@ -6,6 +6,6 @@ export VUE_APP_EVENTS_PATH=`gp url 8080`/events
 export ACAPY_ENDPOINT=`gp url 8030`
 export ACAPY2_ENDPOINT=`gp url 8040`
 export BPA_HOST=`gp url 8080 | sed 's/.*https:\/\///'`
-export PA_WEBHOOK_URL=`gp url 8080`/log
 export BPA2_HOST=`gp url 8090 | sed 's/.*https:\/\///'`
+export BPA_WEBHOOK_URL=`gp url 8080`/log
 export BPA_SECURITY_ENABLED=false
