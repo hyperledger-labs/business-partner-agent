6f409e23b14a2fc24b95617219dc11e06a9b6cc0
Jason Sy <jasyrotuck@gmail.com>
2021-06-29 14:42:38 -0700

    better layout
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- frontend/src/components/ProofRequest.vue
+++ frontend/src/components/ProofRequest.vue
@@ -7,22 +7,36 @@
 -->
 
 <template>
-  <div>
+  <v-container>
     <br />
     <v-row v-for="(value, key) in proofRequest.requestedAttributes" :key="key">
-      <v-col cols="4">
-        <v-row v-for="name in value.names" :key="name">
-          {{ name }}
+      <v-col cols="4" class="pb-0">
+        <v-row>
+          <p class="grey--text text--darken-2 font-weight-medium">
+            Requested Fields
+          </p>
+        </v-row>
+        <v-row v-for="name in value.names" :key="name" style="height: 30px">
+          - {{ name }}
         </v-row>
       </v-col>
-      <v-col cols="4">
-        <v-row v-for="(value, restrict) in value.restrictions[0]" :key="value">
-          {{ restrict }}:{{ value }}
+      <v-col cols="6" class="pb-0">
+        <v-row>
+          <p class="grey--text text--darken-2 font-weight-medium">
+            Restrictions
+          </p>
+        </v-row>
+        <v-row
+          v-for="(value, restrict) in value.restrictions[0]"
+          :key="value"
+          style="height: 30px"
+        >
+          {{ restrict }} = {{ value }}
         </v-row>
       </v-col>
     </v-row>
     <br />
-  </div>
+  </v-container>
 </template>
 
 <script>
