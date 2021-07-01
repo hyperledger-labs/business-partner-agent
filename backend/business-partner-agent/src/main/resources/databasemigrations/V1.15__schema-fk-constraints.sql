0350d7d642ed6f4bff2d9fbd610f91e0e98612f0
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-07-01 15:45:32 +0200

    added credential exchange constraints
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

---
+++ backend/business-partner-agent/src/main/resources/databasemigrations/V1.15__schema-fk-constraints.sql
@@ -0,0 +1,36 @@
+-- bpa_cred_def constraints
+
+ALTER TABLE bpa_cred_def
+    ADD CONSTRAINT bpa_cred_def_schema_fk_1
+        FOREIGN KEY (schema_id)
+            REFERENCES bpaschema(id);
+
+-- bpa_restrictions constraints
+
+ALTER TABLE bpa_restrictions
+    ADD CONSTRAINT bpa_restrictions_schema_fk_1
+        FOREIGN KEY (schema_id)
+            REFERENCES bpaschema(id);
+
+-- delete read only columns
+
+ALTER TABLE bpaschema DROP COLUMN is_read_only;
+ALTER TABLE bpa_restrictions DROP COLUMN is_read_only;
+
+-- bpa_credential_exchange constraints
+
+ALTER TABLE bpa_credential_exchange
+    ADD CONSTRAINT bpa_credential_exchange_partner_fk_1
+        FOREIGN KEY (partner_id)
+            REFERENCES partner(id)
+            ON DELETE CASCADE;
+
+ALTER TABLE bpa_credential_exchange
+    ADD CONSTRAINT bpa_credential_exchange_bpa_cred_def_fk_1
+        FOREIGN KEY (cred_def_id)
+            REFERENCES bpa_cred_def(id);
+
+ALTER TABLE bpa_credential_exchange
+    ADD CONSTRAINT bpa_credential_exchange_schema_fk_1
+        FOREIGN KEY (schema_id)
+            REFERENCES bpaschema(id);
