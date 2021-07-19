9741718f6879b001eda98958d870ca320d9f5357
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-07-19 12:31:39 +0200

    added field to update script.
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- backend/business-partner-agent/src/main/resources/databasemigrations/V1.16__add-partner-trustping.sql
+++ backend/business-partner-agent/src/main/resources/databasemigrations/V1.16__add-partner-trustping.sql
@@ -1,3 +1,5 @@
 ALTER TABLE partner ADD COLUMN trust_ping boolean;
+ALTER TABLE partner ADD COLUMN trust_ping_state character varying(255);
 ALTER TABLE partner ADD COLUMN invitation_msg_id character varying(255);
+
 UPDATE partner set trust_ping = true;
