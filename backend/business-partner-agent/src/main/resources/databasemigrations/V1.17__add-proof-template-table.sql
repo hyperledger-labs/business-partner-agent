f795f14b289b3022c91f7d792aaf1f0b34a10037
Lars Wegner <lars.wegner2@de.bosch.com>
2021-07-27 12:15:40 +0200

    Renamed migration script due to merge from master.
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- backend/business-partner-agent/src/main/resources/databasemigrations/V1.17__add-proof-template-table.sql
+++ backend/business-partner-agent/src/main/resources/databasemigrations/V1.17__add-proof-template-table.sql
@@ -6,11 +6,6 @@ CREATE TABLE bpa_proof_template (
 );
 
 ALTER TABLE partner_proof ADD COLUMN proof_template_id uuid; -- field name plus _id
-DELETE FROM bpa_proof_template AS template
-WHERE NOT EXISTS (
-        SELECT FROM partner_proof proof
-        WHERE proof.proof_template_id = template.id
-    );
 
 ALTER TABLE partner_proof
     ADD CONSTRAINT partner_proof_template_fk_1
