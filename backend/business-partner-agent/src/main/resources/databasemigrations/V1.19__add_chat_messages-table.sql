cf852f3e9d008c201376eb744fd234c87b0ace6a
Jason Sherman <jsherman@parcsystems.ca>
2021-08-12 11:33:30 -0700

    refactor migration version numbers
    Signed-off-by: Jason Sherman <jsherman@parcsystems.ca>

--- backend/business-partner-agent/src/main/resources/databasemigrations/V1.19__add_chat_messages-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE chat_message (
-    id uuid PRIMARY KEY,
-    partner_id uuid NOT NULL,
-    content text,
-    incoming boolean NOT NULL,
-    created_at timestamp without time zone
-);
