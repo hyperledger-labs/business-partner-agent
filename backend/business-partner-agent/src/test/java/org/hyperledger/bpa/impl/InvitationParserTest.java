f4c7a1f470c39edb076123f4234168f2afba2dc5
Jason Sherman <jsherman@parcsystems.ca>
2021-09-20 10:49:19 -0700

    Add Unit Test cases for didcomm invitation types
    Signed-off-by: Jason Sherman <jsherman@parcsystems.ca>

--- backend/business-partner-agent/src/test/java/org/hyperledger/bpa/impl/InvitationParserTest.java
+++ backend/business-partner-agent/src/test/java/org/hyperledger/bpa/impl/InvitationParserTest.java
@@ -45,6 +45,32 @@ public class InvitationParserTest {
         Assertions.assertNotNull(invitation.getInvitation());
     }
 
+    @Test
+    void TestParseReceiveDidCommInvitation() {
+        InvitationParser p = new InvitationParser();
+        p.setMapper(new ObjectMapper());
+
+        InvitationParser.Invitation invitation = p.parseInvitation(this.didCommInvitation);
+        Assertions.assertFalse(invitation.isOob());
+        Assertions.assertTrue(invitation.isParsed());
+        Assertions.assertNotNull(invitation.getInvitation());
+    }
+
+    @Test
+    void TestParseReceiveOOBDidCommInvitation() {
+        InvitationParser p = new InvitationParser();
+        p.setMapper(new ObjectMapper());
+
+        InvitationParser.Invitation invitation = p.parseInvitation(didCommOob);
+        Assertions.assertTrue(invitation.isOob());
+        Assertions.assertTrue(invitation.isParsed());
+        Assertions.assertNotNull(invitation.getInvitation());
+    }
+
     private final String invitation = "ewogICAgIkB0eXBlIjogImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMC9pbnZpdGF0aW9uIiwKICAgICJAaWQiOiAiNGQ1OGJhZjktZDIwOS00MTE4LThkOTQtNGE0OTBlNGEwNGFhIiwKICAgICJzZXJ2aWNlRW5kcG9pbnQiOiAiaHR0cDovL2hvc3QuZG9ja2VyLmludGVybmFsOjgwMzAiLAogICAgInJlY2lwaWVudEtleXMiOiBbCiAgICAgICAgIjZCTlF1dFJIalNWNmJwQ0E2djVkRVB2NW12dWlRS2hyc256cEN4dUgzdXdqIgogICAgXSwKICAgICJsYWJlbCI6ICJCdXNpbmVzcyBQYXJ0bmVyIEFnZW50IDEiCn0=";
     private final String oob = "eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9vdXQtb2YtYmFuZC8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiMmZhYmJhNzYtZTlhNy00Yzk4LTg2ZjMtMTFkNGE1MTYzYjQyIiwgImhhbmRzaGFrZV9wcm90b2NvbHMiOiBbImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2RpZGV4Y2hhbmdlLzEuMCJdLCAic2VydmljZXMiOiBbImRpZDpzb3Y6RXJhWUNESlVQc0NoYmt3N1MxdlY5NiJdLCAibGFiZWwiOiAiYm9iIn0=";
+
+    private final String didCommInvitation = "eyJAdHlwZSI6ICJodHRwczovL2RpZGNvbW0ub3JnL2Nvbm5lY3Rpb25zLzEuMC9pbnZpdGF0aW9uIiwgIkBpZCI6ICJkNGE5ZmY4YS1jNjlmLTRiMWQtODJlYi04NzQwYWRiMzE0MmEiLCAic2VydmljZUVuZHBvaW50IjogImh0dHBzOi8vaW52aXRlMS1icGEtYWNhcHktZGV2LmFwcHMuc2lsdmVyLmRldm9wcy5nb3YuYmMuY2EiLCAibGFiZWwiOiAiaW52aXRlMSIsICJyZWNpcGllbnRLZXlzIjogWyI5MnV2TTFFOG9RbXFUNGZLZkdtam5UTndiandqYUZXYVRpRmtMZXNvbnhSVCJdfQ==";
+    private final String didCommOob = "eyJAdHlwZSI6ICJodHRwczovL2RpZGNvbW0ub3JnL291dC1vZi1iYW5kLzEuMC9pbnZpdGF0aW9uIiwgIkBpZCI6ICI2ZmYzY2UzNy1kYjM1LTRjYTctYTNkOS03MWJmNGYxYzhkYzQiLCAibGFiZWwiOiAiaW52aXRlMSIsICJzZXJ2aWNlcyI6IFsiZGlkOnNvdjpXc3FWaW4xWjRZdnZiODdzU1E3QzJtIl0sICJoYW5kc2hha2VfcHJvdG9jb2xzIjogWyJodHRwczovL2RpZGNvbW0ub3JnL2RpZGV4Y2hhbmdlLzEuMCJdfQ==";
+
 }
