af2f5d0e8ae7e07a9025ce4248e204243bcbdf9e
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-09-22 10:49:37 +0200

    forgot to update type on credential exchange
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- backend/business-partner-agent/src/main/resources/databasemigrations/V1.24__manual-credential-proposal.sql
+++ backend/business-partner-agent/src/main/resources/databasemigrations/V1.24__manual-credential-proposal.sql
@@ -3,3 +3,4 @@ ALTER TABLE bpa_credential_exchange ADD COLUMN state_to_timestamp jsonb;
 ALTER TABLE bpa_credential_exchange ADD COLUMN error_msg character varying(255);
 ALTER TABLE bpa_credential_exchange ALTER COLUMN schema_id DROP NOT NULL;
 ALTER TABLE bpa_credential_exchange ALTER COLUMN cred_def_id DROP NOT NULL;
+UPDATE bpa_credential_exchange SET type = 'INDY' WHERE type = 'SCHEMA_BASED';
