41808723aa21f698bcdaadaf323ae73f4dea6eb1
Tim Schlagenhaufer <tim.schlagenhaufer@bosch.io>
2021-12-03 10:50:01 +0100

    Introduce TypeScript
    Signed-off-by: Tim Schlagenhaufer <tim.schlagenhaufer@bosch.io>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,16 +9,18 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true,
   },
   plugins: ["unicorn"],
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
     "plugin:unicorn/recommended",
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
--- frontend/.husky/pre-commit.sh
+++ frontend/.husky/pre-commit.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Copyright (c) 2020-2021 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository at
+# https://github.com/hyperledger-labs/business-partner-agent
+#
+# SPDX-License-Identifier: Apache-2.0
 . "$(dirname "$0")/_/husky.sh"
 
 cd ./frontend && npx lint-staged
