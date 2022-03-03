8b7b22a4c2ac08d956a58e6e94446e22d1cead09
Tim Schlagenhaufer <tim.schlagenhaufer@bosch.io>
2022-03-03 16:29:18 +0100

    Update local scenario files
    Signed-off-by: Tim Schlagenhaufer <tim.schlagenhaufer@bosch.io>

--- scripts/scenarios/local-network/start_infra.sh
+++ scripts/scenarios/local-network/start_infra.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-#DL, BUILD, RUN, LOCAL HYPERLEDGER
+mkdir resolver
+
+# DL, BUILD, RUN, LOCAL HYPERLEDGER
 git clone https://github.com/bcgov/von-network.git von-network
 ./von-network/manage build
 ./von-network/manage start
 
-#GET LOCAL LEDGER GENESIS FILES AND START RESOLVER
+# GET LOCAL LEDGER GENESIS FILES AND START RESOLVER
 curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o resolver/localhost_9000.txn
 
 while [ $? -ne 0 ];
