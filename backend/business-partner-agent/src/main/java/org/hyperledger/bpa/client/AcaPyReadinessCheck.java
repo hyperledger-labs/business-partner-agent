755c846365af964e2c79a3fa4d8a2dda210624a1
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-06-08 12:39:49 +0200

    revert, because of wrong check
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyReadinessCheck.java
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/client/AcaPyReadinessCheck.java
@@ -25,9 +25,11 @@ import io.micronaut.management.health.indicator.annotation.Readiness;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.hyperledger.aries.AriesClient;
+import org.hyperledger.aries.api.server.AdminStatusReadiness;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Optional;
 
 @Singleton
 @Readiness
@@ -40,11 +42,12 @@ public class AcaPyReadinessCheck extends AbstractHealthIndicator<Map<String, Str
     @Override
     protected Map<String, String> getHealthInformation() {
         try {
-            ac.statusReady().ifPresentOrElse(status -> {
-                if (status.isReady()) {
-                    this.healthStatus = HealthStatus.UP;
-                }
-            }, () -> this.healthStatus = HealthStatus.DOWN);
+            Optional<AdminStatusReadiness> status = ac.statusReady();
+            if (status.isPresent() && status.get().isReady()) {
+                this.healthStatus = HealthStatus.UP;
+            } else {
+                this.healthStatus = HealthStatus.DOWN;
+            }
         } catch (IOException e) {
             this.healthStatus = HealthStatus.DOWN;
         }
