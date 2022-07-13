b28705480c2e8b1b15284b2a0516d2a7b27afad4
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-07-13 17:03:02 +0200

    predicate mapping
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- backend/business-partner-agent/src/main/java/org/hyperledger/bpa/impl/aries/jsonld/LDConverter.java
+++ backend/business-partner-agent/src/main/java/org/hyperledger/bpa/impl/aries/jsonld/LDConverter.java
@@ -18,15 +18,17 @@
 package org.hyperledger.bpa.impl.aries.jsonld;
 
 import com.google.gson.JsonObject;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.util.CollectionUtils;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
+import org.hyperledger.acy_py.generated.model.IndyProofReqPredSpec;
 import org.hyperledger.aries.api.present_proof.PresentProofRequest;
+import org.hyperledger.aries.api.present_proof_v2.DIFField;
 import org.hyperledger.aries.api.present_proof_v2.V2DIFProofRequest;
 
-import java.util.AbstractMap;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class LDConverter {
@@ -34,8 +36,85 @@ public class LDConverter {
     private static final String PATH = "$.credentialSubject.";
 
     public static PresentProofRequest.ProofRequest difToIndyProofRequest(@NonNull V2DIFProofRequest proofRequest) {
-        Map<String, PresentProofRequest.ProofRequest.ProofRequestedAttributes> requestedAttributes = proofRequest
-                .getPresentationDefinition().getInputDescriptors().stream().map(id -> {
+        Map<String, PresentProofRequest.ProofRequest.ProofRequestedAttributes> requestedAttributes = new HashMap<>();
+        Map<String, PresentProofRequest.ProofRequest.ProofRequestedPredicates> requestedPredicates = new HashMap<>();
+
+        List<V2DIFProofRequest.PresentationDefinition.InputDescriptors> inputDescriptors = proofRequest
+                .getPresentationDefinition() != null
+                        ? proofRequest.getPresentationDefinition().getInputDescriptors()
+                        : null;
+
+        if (inputDescriptors != null) {
+            requestedAttributes = requestedAttributesFromInputDescriptor(inputDescriptors);
+            requestedPredicates = requestedPredicatesFromInputDescriptor(inputDescriptors);
+        }
+
+        return PresentProofRequest.ProofRequest
+                .builder()
+                .name(proofRequest.getPresentationDefinition() != null
+                        ? proofRequest.getPresentationDefinition().getName()
+                        : null)
+                .requestedAttributes(requestedAttributes)
+                .requestedPredicates(requestedPredicates)
+                .build();
+    }
+
+    private static Map<String, PresentProofRequest.ProofRequest.ProofRequestedPredicates> requestedPredicatesFromInputDescriptor(
+            @NonNull List<V2DIFProofRequest.PresentationDefinition.InputDescriptors> ids) {
+        Map<String, PresentProofRequest.ProofRequest.ProofRequestedPredicates> result = new LinkedHashMap<>();
+
+        List<V2DIFProofRequest.PresentationDefinition.InputDescriptors> idsWithFields = ids.stream()
+                .filter(id -> id.getConstraints() != null
+                        && id.getConstraints().getFields() != null)
+                .toList();
+
+        Counter c = new Counter(new AtomicInteger(0));
+
+        idsWithFields.forEach(id -> {
+            String schemaId = getSchemaIdFromInputDescriptor(id);
+            id.getConstraints().getFields().forEach(f -> {
+                DIFField.Filter filter = f.getFilter();
+                if (filter != null) {
+                    TypeToValue typeToValue;
+                    if (filter.getMinimum() != null) {
+                        typeToValue = new TypeToValue(IndyProofReqPredSpec.PTypeEnum.LESS_THAN, filter.getMinimum());
+                    } else if (filter.getMaximum() != null) {
+                        typeToValue = new TypeToValue(IndyProofReqPredSpec.PTypeEnum.GREATER_THAN_OR_EQUAL_TO,
+                                filter.getMaximum());
+                    } else if (filter.getExclusiveMaximum() != null) {
+                        typeToValue = new TypeToValue(IndyProofReqPredSpec.PTypeEnum.LESS_THAN,
+                                filter.getExclusiveMaximum());
+                    } else if (filter.getExclusiveMinimum() != null) {
+                        typeToValue = new TypeToValue(IndyProofReqPredSpec.PTypeEnum.GREATER_THAN,
+                                filter.getExclusiveMinimum());
+                    } else {
+                        typeToValue = new TypeToValue(null, null);
+                    }
+                    List<String> paths = f.getPath().stream()
+                            .map(path -> path.replace(PATH, ""))
+                            .collect(Collectors.toList());
+                    if (CollectionUtils.isNotEmpty(paths)) {
+                        paths.forEach(p -> {
+                            PresentProofRequest.ProofRequest.ProofRequestedPredicates.ProofRequestedPredicatesBuilder rp = PresentProofRequest.ProofRequest.ProofRequestedPredicates
+                                    .builder();
+                            rp.name(p);
+                            rp.pType(typeToValue.type);
+                            rp.pValue(typeToValue.value != null ? typeToValue.value.intValue() : null);
+                            rp.restriction(PresentProofRequest.ProofRequest.ProofRestrictions.builder()
+                                    .schemaId(schemaId).build().toJsonObject());
+                            result.put(id.getId() + "-" + c.counter().incrementAndGet(), rp.build());
+                        });
+                    }
+                }
+            });
+        });
+        return result;
+    }
+
+    private static Map<String, PresentProofRequest.ProofRequest.ProofRequestedAttributes> requestedAttributesFromInputDescriptor(
+            @NonNull List<V2DIFProofRequest.PresentationDefinition.InputDescriptors> ids) {
+        return ids.stream()
+                .map(id -> {
                     PresentProofRequest.ProofRequest.ProofRequestedAttributes ra = PresentProofRequest.ProofRequest.ProofRequestedAttributes
                             .builder()
                             .names(id.getConstraints().getFields().stream()
@@ -45,28 +124,15 @@ public class LDConverter {
                                     .flatMap(Collection::stream)
                                     .collect(Collectors.toList()))
                             .restrictions(restrictionFromInputDescriptor(id))
-                            // TODO predicates
                             .build();
                     return new AbstractMap.SimpleEntry<>(id.getId(), ra);
                 })
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        return PresentProofRequest.ProofRequest
-                .builder()
-                .name(proofRequest.getPresentationDefinition() != null
-                        ? proofRequest.getPresentationDefinition().getName()
-                        : null)
-                .requestedAttributes(requestedAttributes)
-                .build();
     }
 
     private static List<JsonObject> restrictionFromInputDescriptor(
             @NonNull V2DIFProofRequest.PresentationDefinition.InputDescriptors id) {
-        String schemaId = id.getSchema()
-                .stream()
-                .map(V2DIFProofRequest.PresentationDefinition.InputDescriptors.SchemaInputDescriptorUri::getUri)
-                .filter(uri -> !StringUtils.contains(uri, "VerifiableCredential"))
-                .findFirst()
-                .orElse(null);
+        String schemaId = getSchemaIdFromInputDescriptor(id);
         if (id.getConstraints() == null || id.getConstraints().getFields() == null) {
             if (schemaId != null) {
                 return List.of(PresentProofRequest.ProofRequest.ProofRestrictions.builder()
@@ -91,5 +157,22 @@ public class LDConverter {
                 .schemaId(schemaId)
                 .build()
                 .toJsonObject());
+    }
+
+    @Nullable
+    private static String getSchemaIdFromInputDescriptor(
+            @NonNull V2DIFProofRequest.PresentationDefinition.InputDescriptors id) {
+        return id.getSchema()
+                .stream()
+                .map(V2DIFProofRequest.PresentationDefinition.InputDescriptors.SchemaInputDescriptorUri::getUri)
+                .filter(uri -> !StringUtils.contains(uri, "VerifiableCredential"))
+                .findFirst()
+                .orElse(null);
+    }
+
+    record Counter(AtomicInteger counter) {
+    }
+
+    record TypeToValue(IndyProofReqPredSpec.PTypeEnum type, Number value) {
     }
 }
