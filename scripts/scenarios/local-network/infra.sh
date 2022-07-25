d9484806d5bc988e06b6dfb25be45ee0f651e31d
Bjoern Arnold <bjoern.arnold@bosch.io>
2022-07-25 14:08:42 +0200

    New infra script with start/stop/prune commands
    Signed-off-by: Bjoern Arnold <bjoern.arnold@bosch.io>

---
+++ scripts/scenarios/local-network/infra.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+export VON_FOLDER=von-network
+export SCRIPT_PATH=./$VON_FOLDER/manage
+
+# -----------------------------------------------------------------------------------------------------------------
+# Functions:
+# -----------------------------------------------------------------------------------------------------------------
+function toLower() {
+  echo $(echo ${@} | tr '[:upper:]' '[:lower:]')
+}
+
+# =================================================================================================================
+# Usage:
+# -----------------------------------------------------------------------------------------------------------------
+usage () {
+  cat <<-EOF
+
+  Usage: $0 [command]
+
+  Commands:
+  start  - Starts the local infrastructure.
+  
+  stop   - Stops the local infrastructure but does not delete the volumes and containers.
+
+  prune  - Stops the local infrastructure and deletes the volumes and containers.
+
+  status - Prints the status of the local infrastructure.
+EOF
+exit 1
+}
+
+# =================================================================================================================
+# Start:
+# -----------------------------------------------------------------------------------------------------------------
+start () {
+  if [ ! -d resolver ]; 
+    then
+      mkdir resolver
+  fi
+
+  # DL, BUILD, RUN, LOCAL HYPERLEDGER
+  
+  if [ ! -d $VON_FOLDER ]; 
+    then
+      git clone https://github.com/bcgov/von-network.git $VON_FOLDER
+  fi
+
+  # Set node IP to local host internal IP - required when executed from Windows Subsystem Linux (WSL)
+  if grep -q "microsoft" /proc/version; then
+      echo 'Starting in WSL mode...'
+      IP=$(hostname -I | xargs)
+      echo 'Node IP:' $IP
+  fi
+
+  $SCRIPT_PATH build
+  $SCRIPT_PATH start $IP
+
+  # GET LOCAL LEDGER GENESIS FILES AND START RESOLVER
+  curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o resolver/localhost_9000.txn
+
+  while [ $? -ne 0 ];
+  do
+    echo '...waiting for infra to finish startup'
+    sleep 3
+    curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o resolver/localhost_9000.txn
+  done
+}
+
+# =================================================================================================================
+# Stop:
+# -----------------------------------------------------------------------------------------------------------------
+stop () {
+  $SCRIPT_PATH stop
+}
+
+# =================================================================================================================
+# Prune:
+# -----------------------------------------------------------------------------------------------------------------
+prune () {
+  $SCRIPT_PATH down
+}
+
+# =================================================================================================================
+# Status:
+# -----------------------------------------------------------------------------------------------------------------
+status () {
+  $SCRIPT_PATH dockerhost
+  docker ps -a --filter 'name=von-' --format "{{.Names}}	-> {{.State}}"
+}
+
+# =================================================================================================================
+COMMAND=$(toLower ${1})
+shift || COMMAND=usage
+
+case "${COMMAND}" in
+  start|up)
+      start
+    ;;
+  stop)
+      stop
+    ;;
+  prune)
+      prune
+    ;;
+  status)
+      status
+    ;;
+  *)
+      usage
+    ;;
+esac
+
